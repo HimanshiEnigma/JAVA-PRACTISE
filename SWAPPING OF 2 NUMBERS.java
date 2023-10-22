d31e5cf4a21cc50f0cca21c4774dfbb96215944b
himanshiii25 <141846224+himanshiii25@users.noreply.github.com>
2023-10-22 11:23:38 -0700

    Create SWAPPING OF 2 NUMBERS.java

---
+++ SWAPPING OF 2 NUMBERS.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class untitled11 {
+  public static void swap(ArrayList<Integer>List,int idx1,int idx2){
+      int temp=List.get(idx1);
+      List.set(idx1,List.get(idx2));
+      List.set(idx2,temp);
+  }
+  public static void main(String[] args) {
+        ArrayList<Integer>List=new ArrayList<> ();
+        List.add(2);
+        List.add(5);
+        List.add(9);
+        List.add(3);
+        List.add(6);
+        int idx=1,idx2=3;
+      System.out.println(List);
+
+      swap(List,idx,idx2);
+      System.out.println(List);
+
+    }
+}
