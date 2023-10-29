40a6b074e9a193b2fe712c898e6fa274662135b1
Himanshi <141846224+HimanshiEnigma@users.noreply.github.com>
2023-10-29 10:18:59 -0700

    Create monotonic arrayList.java

---
+++ monotonic arrayList.java
@@ -0,0 +1,11 @@
+public boolean isMonotonic(ArrayList<Integer> A) {
+boolean inc = true;
+boolean dec = true;
+for (int i=0; i<A.size()-1; i++) {
+if (A.get(i) > A.get(i+1))
+inc = false;
+if (A.get(i) < A.get(i+1))
+dec = false;
+}
+return inc || dec;
+}
